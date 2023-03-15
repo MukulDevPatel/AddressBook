938ada87a7c3e70397e5f6a67d44a5e40f2bb718
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-15 18:45:30 +0530

    [Mukul Dev] Add: This code for delete data

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -8,6 +8,7 @@ namespace AddressBook
 {
     public class AddressBookMain
     {
+        List<Contact> addressBook = new List<Contact>();
         Contact contact = new Contact();
         public void AddContact()
         {
@@ -27,41 +28,65 @@ namespace AddressBook
             contact.PhoneNumber = Console.ReadLine();
             Console.WriteLine("Enter Email");
             contact.Email = Console.ReadLine();
-        }
-        public void Display()
-        {
-            Console.WriteLine(contact.FirstName+"\n"+contact.LastName+"\n"+contact.Address+"\n"+contact.Address+"\n"+contact.City+"\n"+contact.State+"\n"+contact.Zip+"\n"+contact.PhoneNumber+"\n"+contact.Email);
+            addressBook.Add(contact);
         }
         public void EditContact()
         {
-            Console.WriteLine("Enter option to edit\n1. First Name\n2. Last Name\n3. Address\n4. City\n5. State\n6. ZIP\n7. Phone Number\n8. Email");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
+            Console.WriteLine("Edit using First name");
+            string name = Console.ReadLine();
+            foreach (var data in addressBook)
             {
-                case 1:
-                    contact.FirstName = Console.ReadLine();
-                    break;
-                case 2:
-                    contact.LastName = Console.ReadLine();
-                    break;
-                case 3:
-                    contact.Address = Console.ReadLine();
-                    break;
-                case 4:
-                    contact.City = Console.ReadLine();
-                    break;
-                case 5:
-                    contact.State = Console.ReadLine();
-                    break;
-                case 6:
-                    contact.Zip = Console.ReadLine();
-                    break;
-                case 7:
-                    contact.PhoneNumber = Console.ReadLine();
-                    break;
-                case 8:
-                    contact.Email = Console.ReadLine();
-                    break;
+                if (data.FirstName == name)
+                {
+                    Console.WriteLine("Enter option to edit\n1. First Name\n2. Last Name\n3. Address\n4. City\n5. State\n6. ZIP\n7. Phone Number\n8. Email");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            data.FirstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            data.LastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            data.Address = Console.ReadLine();
+                            break;
+                        case 4:
+                            data.City = Console.ReadLine();
+                            break;
+                        case 5:
+                            data.State = Console.ReadLine();
+                            break;
+                        case 6:
+                            data.Zip = Console.ReadLine();
+                            break;
+                        case 7:
+                            data.PhoneNumber = Console.ReadLine();
+                            break;
+                        case 8:
+                            data.Email = Console.ReadLine();
+                            break;
+                    }
+                }
+            }
+        }
+        public void DeleteContact()
+        {
+            Console.WriteLine("Enter name to delete contact");
+            string name = Console.ReadLine() ;
+            foreach (var data in addressBook)
+            {
+                if (data.FirstName.Equals(name))
+                {
+                    addressBook.Remove(data);
+                }
+            }
+        }
+        public void Display()
+        {
+            foreach (var data in addressBook)
+            {
+                Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -5,20 +5,17 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
+            AddressBookMain addressBook = new AddressBookMain();
             Console.WriteLine("Address Book Entry");
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the option\n1. Add Contact\n2. Edit Contact\n3. Exit.");
+                Console.WriteLine("Enter the option\n1. Add Contact\n2. Edit Contact\n3. Delete Contact\n4. Display\n5. Exit.");
                 int option = Convert.ToInt32(Console.ReadLine());
-                AddressBookMain addressBook = new AddressBookMain();
                 switch (option)
                 {
                     case 1:
-                        for (int i = 0; i < 2; i++)
-                        {
-                            addressBook.AddContact();
-                        }
+                        addressBook.AddContact();
                         addressBook.Display();
                         break;
                      case 2:
@@ -26,6 +23,13 @@ namespace AddressBook
                         addressBook.Display();
                         break;
                     case 3:
+                        addressBook.DeleteContact();
+                        addressBook.Display();
+                        break;
+                    case 4:
+                        addressBook.Display();
+                        break;
+                    case 5:
                         flag = false;
                         break;
                         
