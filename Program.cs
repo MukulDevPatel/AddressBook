88cff88ec196845547e995de776932f8d482ddab
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-15 17:14:46 +0530

    [Mukul Dev] Add: This is edit data

--- Program.cs
+++ Program.cs
@@ -5,9 +5,32 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            AddressBookMain addressBook = new AddressBookMain();
-            addressBook.AddContact();
-            addressBook.Display();
+            Console.WriteLine("Address Book Entry");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the option\n1. Add Contact\n2. Edit Contact\n3. Exit.");
+                int option = Convert.ToInt32(Console.ReadLine());
+                AddressBookMain addressBook = new AddressBookMain();
+                switch (option)
+                {
+                    case 1:
+                        for (int i = 0; i < 2; i++)
+                        {
+                            addressBook.AddContact();
+                        }
+                        addressBook.Display();
+                        break;
+                     case 2:
+                        addressBook.EditContact();
+                        addressBook.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                        
+                }
+            }
         }
     }
 }
